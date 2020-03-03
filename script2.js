ea9fdc6f77ad4d46a78bc573331eead9731e7c45
Linus41 <schief.craft@gmail.com>
2020-03-03 15:02:47 -0700

    got questions to replace each other with separate functions

--- script2.js
+++ script2.js
@@ -1,6 +1,9 @@
 // when start button is clicked, a question with multiple choice appears
 document.getElementById("question-one").style.display = "none";
 document.getElementById("question-two").style.display = "none";
+document.getElementById("question-three").style.display = "none";
+document.getElementById("question-four").style.display = "none";
+
 
 function myFunction() {
 
@@ -9,11 +12,31 @@ function myFunction() {
     }
 
 }
-function nextQuestion() {
+// This function only switches question one to question two and no further. Could it be that each switch needs its own function?
+function firstQuestion() {
+
     var quest1 = document.getElementById("question-one").innerHTML;
-    if(quest1) {
+
+    if (quest1) {
         document.getElementById("question-one").style.display = "none";
         document.getElementById("question-two").style.display = "block";
     }
 }
 
+function secondQuestion() {
+
+    var quest2 = document.getElementById("question-two").innerHTML;
+
+    if (quest2) {
+        document.getElementById("question-two").style.display = "none";
+        document.getElementById("question-three").style.display = "block";
+    }  
+}
+
+function thirdQuestion() {
+    var quest3 = document.getElementById("question-three").innerHTML;
+    if (quest3) {
+        document.getElementById("question-three").style.display = "none";
+        document.getElementById("question-four").style.display = "block";
+    }
+}
