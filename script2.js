080927063e19b0d20450af10a7fa9ffd7be4e1de
Linus41 <schief.craft@gmail.com>
2020-03-03 14:39:38 -0700

    got questions to replace themselves!

--- script2.js
+++ script2.js
@@ -1,11 +1,19 @@
 // when start button is clicked, a question with multiple choice appears
-document.getElementById("question").style.display = "none";
-
+document.getElementById("question-one").style.display = "none";
+document.getElementById("question-two").style.display = "none";
 
 function myFunction() {
 
-    if (document.getElementById("question").style.display = "none") {
-        document.getElementById("question").style.display = "block";
+    if (document.getElementById("question-one").style.display = "none") {
+        document.getElementById("question-one").style.display = "block";
     }
 
 }
+function nextQuestion() {
+    var quest1 = document.getElementById("question-one").innerHTML;
+    if(quest1) {
+        document.getElementById("question-one").style.display = "none";
+        document.getElementById("question-two").style.display = "block";
+    }
+}
+
