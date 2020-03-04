e1204d8a3c9ebce8b759d412df00c9a54db1c8bf
Linus41 <schief.craft@gmail.com>
2020-03-04 12:19:13 -0700

    added a score keeping function, deleted extraneous files

--- script2.js
+++ script2.js
@@ -1,10 +1,14 @@
-// when start button is clicked, a question with multiple choice appears
+
 document.getElementById("question-one").style.display = "none";
 document.getElementById("question-two").style.display = "none";
 document.getElementById("question-three").style.display = "none";
 document.getElementById("question-four").style.display = "none";
 
-
+// this var grabs from DOM the div element score
+var scorePoints = document.getElementById("score");
+// declare score variable to store points
+var count = 0;
+// when start button is clicked, a question with multiple choice appears
 function myFunction() {
 
     if (document.getElementById("question-one").style.display = "none") {
@@ -12,7 +16,7 @@ function myFunction() {
     }
 
 }
-// This function only switches question one to question two and no further. Could it be that each switch needs its own function?
+// The following three functions replace current question with next question
 function firstQuestion() {
 
     var quest1 = document.getElementById("question-one").innerHTML;
@@ -40,9 +44,22 @@ function thirdQuestion() {
         document.getElementById("question-four").style.display = "block";
     }
 }
+// This function changes the screen to score element block
+function fourthQuestion() {
+    var quest4 = document.getElementById("question-four").innerHTML;
+    if(quest4) {
+        document.getElementById("question-four").style.display = "none";
+        document.getElementById("score").style.display = "block";
+    }
+}
 function scoreKeep() {
     var correct = document.getElementsByClassName("correct");
     if(correct) {
+        scorePoints = count++;
         
     }
 }
+console.log(scorePoints);
+
+// Questions for Namita: How to keep score? How to display score? How to get timer 
+// to be styled inline (4:00) rather than 4 with 00 underneath?
