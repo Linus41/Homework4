795c3fc6ac97321112d86bc61278a90ddc43888a
Linus41 <schief.craft@gmail.com>
2020-03-04 21:34:40 -0700

    updated so Anthony can check out where I'm at :)

--- script2.js
+++ script2.js
@@ -4,10 +4,10 @@ document.getElementById("question-two").style.display = "none";
 document.getElementById("question-three").style.display = "none";
 document.getElementById("question-four").style.display = "none";
 
-// this var grabs from DOM the div element score
-var scorePoints = document.getElementById("score");
-// declare score variable to store points
-var count = 0;
+
+
+
+
 // when start button is clicked, a question with multiple choice appears
 function myFunction() {
 
@@ -34,7 +34,7 @@ function secondQuestion() {
     if (quest2) {
         document.getElementById("question-two").style.display = "none";
         document.getElementById("question-three").style.display = "block";
-    }  
+    }
 }
 
 function thirdQuestion() {
@@ -47,19 +47,25 @@ function thirdQuestion() {
 // This function changes the screen to score element block
 function fourthQuestion() {
     var quest4 = document.getElementById("question-four").innerHTML;
-    if(quest4) {
+    if (quest4) {
         document.getElementById("question-four").style.display = "none";
         document.getElementById("score").style.display = "block";
     }
 }
-function scoreKeep() {
-    var correct = document.getElementsByClassName("correct");
-    if(correct) {
-        scorePoints = count++;
-        
-    }
-}
-console.log(scorePoints);
+// declare count variable to store points
+count = 0;
 
-// Questions for Namita: How to keep score? How to display score? How to get timer 
-// to be styled inline (4:00) rather than 4 with 00 underneath?
+function correct() {
+    var score = document.getElementsByClassName("correct");
+    if (score) {
+        count = count + 1;
+    }
+
+}
+
+console.log(correct);
+// when correct answer is clicked, function correct fires
+// why doesn't this work???
+
+
+
